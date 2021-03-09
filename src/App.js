848bbd6e6db1a2e9cdad0c49e44f63234d0f95ae
Bobby Willmes <bobbywillmes@gmail.com>
2021-03-08 17:54:43 -0800

    connect to API & build currency dropdown

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 const Header = () => {
   return (
     <div id="header" className="container-fluid">
-      <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1">Currency Exchange</span>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">Currency Exchange</span>
       </nav>
     </div>
   )
@@ -25,15 +25,57 @@ const Footer = () => {
 
 class Main extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      response: {},
+      countries: []
+    }
+  }
+
+  getCountries() {
+    // console.log(`getCountries()`)
+    let rates = this.state.response.rates
+    let keys = Object.keys(rates)
+    this.setState({ countries: keys })
+    console.log(this.state)
+  }
+
+  getData() {
+    fetch(`https://api.exchangeratesapi.io/latest?base=USD`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ response: data })
+      })
+      .then(() => {
+        this.getCountries()
+      })
+  }
+
+  componentDidMount() {
+    this.getData()
   }
 
   render() {
     return (
       <div id="main" className="container">
         <div className="row">
-          <div className="col">(Currency goes here)</div>
-          <div className="col">(converter also might go here)</div>
+          <div className="col">Amount</div>
+          <div className="col">From</div>
+          <div className="col">To</div>
+          <div className="col">New Amount</div>
+        </div>
+        <div className="row">
+          <div className="col">1</div>
+          <div className="col">USD</div>
+          <div className="col">
+            <select id="" className="form-select">
+              <option>Select Currency</option>
+              {this.state.countries.map((value, index) => {
+                return <option key={value}>{value}</option>
+              })}
+            </select>
+          </div>
+          <div className="col">0.84</div>
         </div>
       </div>
     )
